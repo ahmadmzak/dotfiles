ee70c5c84fd77c09764178e8aa81d48d161dc2fe
Ahmed Zakir <10600749+ahmadmzak@users.noreply.github.com>
2025-02-12 02:19:55 +0100

    theme and other stuff

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,9 +1,12 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = "Catppuccin Macchiato"
-config.window_background_opacity = 0.7
-config.font = wezterm.font("JetBrains Mono Nerd Font")
+config.color_scheme = "Catppuccin Mocha"
+-- config.window_background_opacity = 0.9
+config.font = wezterm.font("JetBrains Mono Nerd Font", { weight = "Regular" })
+config.font_size = 11
+config.line_height = 1.2
+config.use_resize_increments = true
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 config.enable_scroll_bar = false
